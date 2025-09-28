f166e17a41c44f655018b65c75330ea8f48bd0be
Ubuntu <ubuntu@ip-172-31-23-178.ec2.internal>
2025-09-28 00:05:58 +0000

    Add Dockerfiles and updated project files for ECS deployment

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,25 @@
-const express = require('express')
+const express = require('express');
 const { v4: uuidv4 } = require('uuid');
-const { CORS_ORIGIN } = require('./config')
-console.log(require('./config'))
-console.log(CORS_ORIGIN)
+const { CORS_ORIGIN } = require('./config');
 
-const ID = uuidv4()
-const PORT = 8080
+const ID = uuidv4();
+const PORT = 8080;
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', CORS_ORIGIN)
-    res.setHeader('Access-Control-Allow-Methods', 'GET')
-    res.setHeader('Access-Control-Allow-Headers', '*')
+    res.setHeader('Access-Control-Allow-Origin', CORS_ORIGIN);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     next();
-})
-app.get(/.*/, (req, res) => {
-    console.log(`${new Date().toISOString()} GET`)
-    res.json({id: ID})
-})
+});
 
-app.listen(PORT, () => {
-    console.log(`Backend started on ${PORT}. ctrl+c to exit`)
-})
+app.get(/.*/, (req, res) => {
+    console.log(`${new Date().toISOString()} GET`);
+    res.json({ id: ID });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Backend started on ${PORT}. ctrl+c to exit`);
+});
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8080/'
-export default API_URL
+export const API_URL = 'http://54.198.181.150:8080';
+export default API_URL;
